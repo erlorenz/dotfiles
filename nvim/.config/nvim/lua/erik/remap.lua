eb5715aaef585e22723e3a7c527ed68239a01454
Erik Lorenz <erik@eriklorenz.dev>
2023-02-26 10:46:39 -0600

    Updated paste functionality in nvim

--- nvim/.config/nvim/lua/erik/remap.lua
+++ nvim/.config/nvim/lua/erik/remap.lua
@@ -1,10 +1,20 @@
+--Map leader and esc
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
 vim.keymap.set("i", "jk", "<Esc>")
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") 
+--Move selected line up and down
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv") 
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv") 
 
+--Better paste (without swap)
+vim.keymap.set("v", "p", '"_dP')
+
+--Delete to void register
+vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>dd", '"_dd')
+vim.keymap.set("n", "x", '"_dl')
+
+--When scrolling center cursor on page
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
